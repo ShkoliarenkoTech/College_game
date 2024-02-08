914335e7e71687f503479a9bec0948ea1285c082
Dima <shkoliarenkod@gmail.com>
2024-02-08 01:49:52 +0100

    6_3

--- js game (1)/js game/main.js
+++ js game (1)/js game/main.js
@@ -1,5 +1,7 @@
 const $btn = document.getElementById('btn-kick')
 const btn_super = document.getElementById('btn-super-kick')
+const logsDiv = document.querySelector('#logs')
+
 
 function renderHP() {
   this.renderHPLife()
@@ -7,6 +9,11 @@ function renderHP() {
 }
 
 function changeHP(count) {
+    const log =
+    this === enemy ? generateLog(this, character) : generateLog(this, enemy)
+  console.log(log)
+
+
   if (this.damageHP < count) {
     this.damageHP = 0
     alert('Бедный ' + this.name + ' проиграл бой!')
@@ -68,3 +75,32 @@ btn_super.addEventListener('click', function () {
 function init() {
   console.log('Start Game!')
 }
+
+
+function generateLog(firstPerson, secondPerson) {
+    const hpLeft = firstPerson.damageHP + '/' + firstPerson.defaultHP
+    const damage = firstPerson.defaultHP - firstPerson.damageHP
+  
+    const logs = [
+      `${firstPerson.name} вспомнил что-то важное, но неожиданно ${secondPerson.name}, не помня себя от испуга, ударил в предплечье врага. -${damage}, [${hpLeft}]`,
+      `${firstPerson.name} поперхнулся, и за это ${secondPerson.name} с испугу приложил прямой удар коленом в лоб врага. -${damage}, [${hpLeft}]`,
+      `${firstPerson.name} забылся, но в это время наглый ${secondPerson.name}, приняв волевое решение, неслышно подойдя сзади, ударил. -${damage}, [${hpLeft}]`,
+      `${firstPerson.name} пришел в себя, но неожиданно ${secondPerson.name} случайно нанес мощнейший удар. -${damage}, [${hpLeft}]`,
+      `${firstPerson.name} поперхнулся, но в это время ${secondPerson.name} нехотя раздробил кулаком <вырезанно цензурой> противника. -${damage}, [${hpLeft}]`,
+      `${firstPerson.name} удивился, а ${secondPerson.name} пошатнувшись влепил подлый удар. -${damage}, [${hpLeft}]`,
+      `${firstPerson.name} высморкался, но неожиданно ${secondPerson.name} провел дробящий удар. -${damage}, [${hpLeft}]`,
+      `${firstPerson.name} пошатнулся, и внезапно наглый ${secondPerson.name} беспричинно ударил в ногу противника. -${damage}, [${hpLeft}]`,
+      `${firstPerson.name} расстроился, как вдруг, неожиданно ${secondPerson.name} случайно влепил стопой в живот соперника. -${damage}, [${hpLeft}]`,
+      `${firstPerson.name} пытался что-то сказать, но вдруг, неожиданно ${secondPerson.name} со скуки, разбил бровь сопернику. -${damage}, [${hpLeft}]`,
+    ]
+  
+    const log = logs[random(logs.length) - 1]
+    const p = document.createElement('p')
+    p.innerText = log
+  
+    logsDiv.insertBefore(p, logsDiv.children[0])
+  
+    return log
+  }
+
+  init()
